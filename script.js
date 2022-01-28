60aaf71a558167ef6af1ab15374232df1daafd8d
Lorena C <lorena@eunknown.org>
2022-01-27 19:52:47 -0800

    create a toggle

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -25,7 +30,13 @@ btnRoll.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
