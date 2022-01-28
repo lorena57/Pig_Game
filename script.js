fc99b721582d0778e639b4a6e7f8bc29daf00867
Lorena C <lorena@eunknown.org>
2022-01-27 19:48:01 -0800

    create else statement for other player

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let currentScore = 0;
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
